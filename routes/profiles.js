1330098dce154163651133b5e15be2ccdff77702
Nirakar Sapkota <sapkotanirakar07@gmail.com>
2019-04-11 00:16:09 -0400

    Update profiles.js

--- routes/profiles.js
+++ routes/profiles.js
@@ -61,10 +61,7 @@ router.post("/", isLoggedIn, function(req, res){
   var q6 = req.body.q6;
   var q7 = req.body.q7;
   var author = req.user.username
-
-
-
-    var newProfile = {q1: q1, q2: q2, q3: q3, q4: q4, q5: q5, q6: q6, q7: q7, author: author}
+  var newProfile = {q1: q1, q2: q2, q3: q3, q4: q4, q5: q5, q6: q6, q7: q7, author: author}
     // Create a new Profile and save to DB
     Profile.create(newProfile, function(err, newlyCreated){
         if(err){
