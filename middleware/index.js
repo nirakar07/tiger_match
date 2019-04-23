84430186b7643e791105d8334097b1aef9d8abe9
parttimedebater <melhabr@yahoo.com>
2019-04-22 23:39:32 -0400

    Update index.js

--- middleware/index.js
+++ middleware/index.js
@@ -17,7 +17,7 @@ module.exports = {
            return next();
       }
        req.flash('error', 'Your account has not been verified!');
-       res.redirect('/login');     
+       //res.redirect('/login');     
       }      
     }
      req.flash('error', 'You must be signed in to do that!');
