467deb7f1b9f07a00383714872ff0d412a656e6b
parttimedebater <melhabr@yahoo.com>
2019-04-22 22:17:13 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ var UserSchema = new mongoose.Schema({
     password: String,
     isAdmin: {type: Boolean, default: false},
     verified: {type: Boolean, default: false},
-    hash: Number // Only catches first 64 bits of MD5 hash
+    verifyHash: Number // Only catches first 64 bits of MD5 hash
 });
     
 
