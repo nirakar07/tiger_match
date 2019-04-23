99aaa219ec40f8db5c7facb45e44414865bb0ef5
parttimedebater <melhabr@yahoo.com>
2019-04-22 22:10:41 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,7 +4,9 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String,
-    isAdmin: {type: Boolean, default: false}
+    isAdmin: {type: Boolean, default: false},
+    verified: {type: Boolean, default: false},
+    hash: Number // Only catches first 64 bits of MD5 hash
 });
     
 
