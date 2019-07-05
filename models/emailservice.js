1efa4032e2056dac41ae91bfee83f7b5bfb61289
parttimedebater <melhabr@yahoo.com>
2019-07-04 21:23:16 -0400

    Update emailservice.js

--- models/emailservice.js
+++ models/emailservice.js
@@ -1,9 +1,20 @@
 const nodemailer = require("nodemailer");
 
-exports.transporter = nodemailer.createTransport({
+let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: 'tigermatch19@gmail.com', 
       pass: 'thisisthetigermatchpassword' 
     }
   });
+
+exports.sendVerificationEmail = function(netid) {
+    
+    let info = await transporter.sendMail({
+        from: '"TigerMatch" <tigermatch19@gmail.com>', // sender address
+        to: netid + "@princeton.edu", // list of receivers
+        subject: "Test email", // Subject line
+        text: "It works!", // plain text body
+    });
+    
+}
