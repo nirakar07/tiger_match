6d4c6e15ade6033c7ab4d610434f79bf67f5574b
parttimedebater <melhabr@yahoo.com>
2019-07-04 21:54:58 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -51,6 +51,7 @@ router.post("/verify", function(req, res){
     else {
         EmailService.sendVerificiationEmail(req.user.username);
     }
+    res.redirect("/verify");
 });
 
 //handling login logic
