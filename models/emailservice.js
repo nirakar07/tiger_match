c6ab477350cb344c8635c7558a1089c2f3bbc23f
parttimedebater <melhabr@yahoo.com>
2019-07-07 15:38:25 -0400

    Update emailservice.js

--- models/emailservice.js
+++ models/emailservice.js
@@ -10,13 +10,13 @@ let transporter = nodemailer.createTransport({
 
 exports.sendVerificationEmail = function(req, res) {
     
-    console.log(req.user);
+    console.log(req.user.username);
     
-    //let info = transporter.sendMail({
-    //    from: '"TigerMatch" <tigermatch19@gmail.com>',
-    //    to: req.user.username + "@princeton.edu",
-    //    subject: "TigerMatch Verification",
-    //    text: "Welcome to TigerMatch! Verify your email using this link: \nhttp:\/\/" + req.headers.host + "\/verify?token=" + req.user.verifyHash + ".\n"
-    //});
+    let info = transporter.sendMail({
+        from: '"TigerMatch" <tigermatch19@gmail.com>',
+        to: req.user.username + "@princeton.edu",
+        subject: "TigerMatch Verification",
+        text: "Welcome to TigerMatch! Verify your email using this link: \nhttp:\/\/" + req.headers.host + "\/verify?token=" + req.user.verifyHash + ".\n"
+    });
     
 }
